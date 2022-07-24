d8c69411da32880f8d8489dbffb8ed743b476915
Sangryul Ji <sji@c7r1s10.42seoul.kr>
2022-07-24 17:22:32 +0900

    [BOJ]2753: 윤년

---
+++ 2753/2753.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+	if (a % 4 == 0)
+	{
+		if (a % 400 == 0)
+		{
+			printf("1\n");
+			return 0;
+		}
+		else if (a % 100 == 0)
+		{
+			printf("0\n");
+			return 0;
+		}
+		printf("1\n");
+		return 0;
+	}
+	printf("0\n");
+	return 0;
+}
